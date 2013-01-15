1ba3334cdcffa6cb04d152a360ddfd66615c70df
Ben Pellow <benjamin.pellow@gmail.com>
2013-01-14 19:52:25 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfileBuddyServer::Application.config.secret_token = '3055f0136a181e066c849412cb512d2869c0506bd3aba20fe86567e22dd228c66a72c5920d6ba1d9653db260f6c3d12563af8a1e4df70731ab6c2907cd2eb3b1'
