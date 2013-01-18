799d7dfd8a571b15468ae6881fa48aaec20c58b5
Ben Pellow <benjamin.pellow@gmail.com>
2013-01-17 20:57:01 -0800

    include profiles in user json

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   attr_accessible :username
 
   has_many :profiles
+
+  def serializable_hash(options={})
+    options ||= {}
+    options = {
+      :include => [:profiles]
+    }.update(options)
+
+    super(options)
+  end
 end
